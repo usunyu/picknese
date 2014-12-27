d7640e2de1a7e5932132b3b60b993964aff9dd48
Yu Sun <usunyu@gmail.com>
2014-12-27 02:32:01 -0800

    extend user signup

--- django-env/picknese/picknese/forms.py
+++ django-env/picknese/picknese/forms.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class PickneseSignUpForm(UserCreationForm):
+	first_name = forms.CharField(required=True)
+	last_name = forms.CharField(required=True)
 	email = forms.EmailField(required=True)
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password1', 'password2')
+		fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
 
 	def save(self, commit=True):
 		user = super(UserCreationForm, self).save(commit=False)
+		user.first_name = self.cleaned_data['first_name']
+		user.last_name = self.cleaned_data['last_name']
 		user.email = self.cleaned_data['email']
 
 		if commit:
--- django-env/picknese/picknese/views.py
+++ django-env/picknese/picknese/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from forms import PickneseSignUpForm
 
+@login_required(login_url='/accounts/login/')
 def index(request):
 	return render(request, 'index.html')
 
