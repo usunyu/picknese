7fc710af2e3b37f84327553f0fbb0ddf0a907c67
Yu Sun <usunyu@gmail.com>
2014-12-31 22:40:11 -0800

    register admin PickProvider

--- django-env/picknese/pickup/admin.py
+++ django-env/picknese/pickup/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from pickup.models import PickUp
+from pickup.models import PickUp, PickProvider
 
 # Register your models here.
 admin.site.register(PickUp)
+admin.site.register(PickProvider)
