01e28f06db20395e1bf90d6dc1c71e28900432fb
Yu Sun <usunyu@gmail.com>
2015-01-03 18:16:16 -0800

    update university page

--- django-env/picknese/university/urls.py
+++ django-env/picknese/university/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^(?P<university_id>\d+)/$', 'university.views.university'),
+	url(r'^home/(?P<university_id>\d+)/$', 'university.views.university'),
 )
