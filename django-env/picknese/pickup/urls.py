0c743cec2dbad1e0d556eb5f723430de1bff1c9c
Yu Sun <usunyu@gmail.com>
2015-01-06 12:52:32 -0800

    change providers urls

--- django-env/picknese/pickup/urls.py
+++ django-env/picknese/pickup/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^provider/(?P<university_id>\d+)/$', 'pickup.views.pick_provider_list'),
+	url(r'^providers/(?P<university_id>\d+)/$', 'pickup.views.pick_provider_list'),
 	url(r'^provider/create/$', 'pickup.views.provide_pick_provider'),
 	url(r'^provider/delete/(?P<university_id>\d+)/$', 'pickup.views.cancel_pick_provider'),
 	url(r'^create/(?P<university_id>\d+)/$', 'pickup.views.request_pickup'),
