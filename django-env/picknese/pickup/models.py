5f38f50048685aab56506883de8669db5b8d463f
Yu Sun <usunyu@gmail.com>
2015-01-08 23:44:55 -0800

    add PickRequester

--- django-env/picknese/pickup/models.py
+++ django-env/picknese/pickup/models.py
@@ -5,17 +5,29 @@ from university.models import University
 class PickProvider(models.Model):
 	picker = models.ForeignKey(User)
 	university = models.ForeignKey(University)
+	# TODO: delete
 	price = models.IntegerField(default=20)
-	# provider description
+	# TODO: delete
 	description = models.TextField(null=True, blank=True)
-	# TODO: check listed, user can unlist
+	# TODO: check listed, user can unlist, when listed, notify
 	listed = models.BooleanField(default=True)
 
 	class Meta:
 		unique_together = (("picker", "university"),)
 
 	def __str__(self):	# __unicode__ on Python 2
-		return self.picker.username + ' : ' + self.university.name
+		return 'Picker: %s, University: %s' % (self.picker.username, self.university)
+
+class PickRequester(models.Model):
+	requester = models.ForeignKey(User)
+	university = models.ForeignKey(University)
+	pick_type = models.IntegerField()
+	flight = models.CharField(max_length=20, null=True, blank=True)
+	confirmed = models.BooleanField(default=False)
+	description = models.TextField(null=True, blank=True)
+
+	def __str__(self):	# __unicode__ on Python 2
+		return 'Requester: %s, University: %s' % (self.requester.username, self.university)
 
 class PickUp(models.Model):
 	picker = models.ForeignKey(User, related_name='pickup_picker')
@@ -30,4 +42,4 @@ class PickUp(models.Model):
 		unique_together = (("picker", "pickee", "flight"),)
 
 	def __str__(self):	# __unicode__ on Python 2
-		return '%s : %s' % (self.picker.username, self.pickee.username)
+		return 'Picker: %s, Pickee: %s' % (self.picker.username, self.pickee.username)
