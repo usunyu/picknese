4e8782193f3fffe7431edcdf0be6b9aae35c492a
Yu Sun <usunyu@gmail.com>
2015-01-11 00:02:14 -0800

    flight# & feed layout

--- django-env/picknese/pickup/models.py
+++ django-env/picknese/pickup/models.py
@@ -27,7 +27,7 @@ class PickRequester(models.Model):
 	)
 	pick_type = models.IntegerField(choices=type_choices, default=1)
 	price = models.IntegerField(default=20)
-	flight = models.CharField(max_length=20, null=True, blank=True)
+	flight = models.CharField('Flight#', max_length=20, null=True, blank=True)
 	destination = models.CharField(max_length=200, default='Near Campus')
 	confirmed = models.BooleanField(default=False)
 	description = models.TextField(null=True, blank=True)
@@ -44,7 +44,7 @@ class PickUp(models.Model):
 		(2, 'General'),
 	)
 	pick_type = models.IntegerField(choices=type_choices, default=1)
-	flight = models.CharField(max_length=20, null=True, blank=True)
+	flight = models.CharField('Flight#', max_length=20, null=True, blank=True)
 	price = models.IntegerField(default=20)
 	destination = models.CharField(max_length=200, default='Near Campus')
 	# requester description
