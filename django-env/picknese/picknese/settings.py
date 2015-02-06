5331891cb952c033361865d3e7ce14d4ed4f384c
Yu Sun <usunyu@gmail.com>
2015-02-06 02:21:31 -0800

    [api] setting TokenAuthentication

--- django-env/picknese/picknese/settings.py
+++ django-env/picknese/picknese/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'bootstrap3',
     'userprofile',
     'story',
@@ -119,6 +120,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
