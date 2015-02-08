5dfa9e04ef7a0894d9701ade38672f9d9ac927d0
Yu Sun <usunyu@gmail.com>
2015-02-08 15:21:32 -0800

    [react] add fade in effect & seperate user panel

--- django-env/picknese/pickup/static/js/pickup_requester_list.js
+++ django-env/picknese/pickup/static/js/pickup_requester_list.js
@@ -58,7 +58,7 @@ var PickRequester = React.createClass({
         var price = this.props.pickRequester.price;
         var description = this.props.pickRequester.description;
         return (
-            <div className="panel panel-default">
+            <div className="panel panel-default fadein-effect">
                 <div className="panel-body">
                 <div className="col-xs-12 col-sm-3 col-md-2 col-lg-2">
                     <img
@@ -187,6 +187,55 @@ var PickRecordList = React.createClass({
     }
 });
 
+var CurrentUserPanel = React.createClass({
+    render: function() {
+        if (!this.props.currentUser || !this.props.currentUser.id) {
+            return (
+                <div className="panel panel-default">
+                    <div className="panel-body">
+                        <hr />
+                        <p>Hello World</p>
+                    </div>
+                    <hr />
+                </div>
+            );
+        }
+        var currentUser = this.props.currentUser;
+        var avatar = currentUser.profile.avatar;
+        return (
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-6 col-md-6">
+                            <img
+                                className="img-circle box-shadow"
+                                src={avatar ? avatar : '/media/images/avatar/default_pic.png'}
+                                style={{width: '100px', height: '100px'}} />
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-6">
+                            <p style={{marginTop: '10px', marginBottom: '-10px'}}>
+                                <b>{currentUser.first_name} {currentUser.last_name}</b>
+                            </p>
+                            <hr />
+                            <p style={{marginTop: '-10px'}}>
+                                <a href="#">Edit Profile</a>
+                            </p>
+                        </div>
+                    </div>
+                    <hr />
+                    <button
+                        type="button"
+                        className="btn btn-primary col-xs-12 col-sm-12 col-md-12" >
+                        <i className="glyphicon glyphicon-send"></i>&nbsp;
+                        Post Your Request
+                    </button>
+                </div>
+                <hr />
+            </div>
+        );
+    }
+});
+
 var PickRequesterPanel = React.createClass({
     loadPickRequestersFromServer: function() {
         var universityID = parseLastNumberInURLPath();
@@ -307,7 +356,7 @@ var PickRequesterPanel = React.createClass({
         return (
             <div className="row col-md-12"
                  style={{marginTop: '10px'}}>
-                <div className="col-xs-12 col-sm-4 col-md-3">
+                <div className="col-xs-12 col-sm-4 col-md-3 fadein-effect">
                     <CurrentUserPanel
                         currentUser={this.state.currentUser} />
                 </div>
@@ -317,7 +366,7 @@ var PickRequesterPanel = React.createClass({
                         requesters={this.state.requesters}
                         handlePickupSubmit={this.handlePickupSubmit} />
                 </div>
-                <div className="col-xs-12 col-sm-6 col-md-2 sidebar-offcanvas">
+                <div className="col-xs-12 col-sm-6 col-md-2 sidebar-offcanvas fadein-effect">
                     <PickRecordList
                         pickups={this.state.pickups} />
                 </div>
--- django-env/picknese/university/static/js/university.js
+++ django-env/picknese/university/static/js/university.js
@@ -1,3 +1,46 @@
+var CurrentUserPanel = React.createClass({
+    render: function() {
+        if (!this.props.currentUser || !this.props.currentUser.id) {
+            return (
+                <div className="panel panel-default">
+                    <div className="panel-body">
+                        <hr />
+                        <p>Hello World</p>
+                    </div>
+                    <hr />
+                </div>
+            );
+        }
+        var currentUser = this.props.currentUser;
+        var avatar = currentUser.profile.avatar;
+        return (
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-6 col-md-6">
+                            <img
+                                className="img-circle box-shadow"
+                                src={avatar ? avatar : '/media/images/avatar/default_pic.png'}
+                                style={{width: '100px', height: '100px'}} />
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-6">
+                            <p style={{marginTop: '10px', marginBottom: '-10px'}}>
+                                <b>{currentUser.first_name} {currentUser.last_name}</b>
+                            </p>
+                            <hr />
+                            <p style={{marginTop: '-10px'}}>
+                                <a href="#">Edit Profile</a>
+                            </p>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+                <hr />
+            </div>
+        );
+    }
+});
+
 var UniversityPanel = React.createClass({
     loadUniversityFromServer: function() {
         var universityID = parseLastNumberInURLPath();
@@ -43,11 +86,11 @@ var UniversityPanel = React.createClass({
         return (
             <div className="row col-md-12"
                  style={{marginTop: '10px'}}>
-                <div className="col-xs-12 col-sm-4 col-md-3">
+                <div className="col-xs-12 col-sm-4 col-md-3 fadein-effect">
                     <CurrentUserPanel
                         currentUser={this.state.currentUser} />
                 </div>
-                <div className="col-xs-12 col-sm-8 col-md-7">
+                <div className="col-xs-12 col-sm-8 col-md-7 fadein-effect">
                     <p><b>Description:</b> &nbsp;{university.description}</p>
                     <p><b>Address:</b> &nbsp;{university.address}</p>
                     <p><b>State:</b> &nbsp;{university.state}</p>
