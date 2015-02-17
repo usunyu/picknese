cf7dc74098a0f6a537ca2f8c8868beeee734e776
Yu Sun <usunyu@gmail.com>
2015-02-17 04:09:28 -0800

    [git] remove useless gitignore

---
+++ django-env/static/js/component/CurrentUserPanel.js
@@ -0,0 +1,49 @@
+var CurrentUserPanel = React.createClass({
+    render: function() {
+        if (!this.props.currentUser || !this.props.currentUser.id) {
+            return (
+                <div className="panel panel-default">
+                    <div className="panel-body">
+                        <hr />
+                        <p>Hello World, please login :)</p>
+                    </div>
+                    <hr />
+                </div>
+            );
+        }
+        var currentUser = this.props.currentUser;
+        var avatar = currentUser.profile.avatar;
+        return (
+            <div className="panel panel-primary clearfix">
+                <div className="panel-heading" style={{minHeight: '80px'}}></div>
+                <div className="panel-body" style={{marginTop: '-70px'}}>
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-6 col-md-6">
+                            <img
+                                className="img-thumbnail"
+                                src={avatar ? avatar : getProfileDefaultPic()}
+                                style={{width: '100px', height: '100px'}} />
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-6" style={{marginTop: '25px'}}>
+                            <p className="color-white">
+                                <b>{currentUser.first_name} {currentUser.last_name}</b>
+                            </p>
+                            <p>
+                                <a href="/accounts/profile/">Edit Profile</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="list-group">
+                    <a href="#" className="list-group-item">
+                        <span className="badge">14</span>
+                        <i className="glyphicon glyphicon-plane"></i> &nbsp; My Pick Up List
+                    </a>
+                    <a href="#" className="list-group-item">
+                        <i className="glyphicon glyphicon-road"></i> &nbsp; My Carpool List
+                    </a>
+                </div>
+            </div>
+        );
+    }
+});
