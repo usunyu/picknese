e9b2e0ff64a2688759e93c5a7ef25ee019042e1b
Yu Sun <usunyu@gmail.com>
2015-04-11 23:01:51 -0700

    [model] add user contact info

--- django-env/userprofile/models.py
+++ django-env/userprofile/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from university.models import University
+
 from picknese import settings
 from picknese.utils import get_upload_file_name
 
-# Create your models here.
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	gender_choices = (
@@ -24,6 +25,10 @@ class UserProfile(models.Model):
 		null=True,
 		blank=True
 	)
+	# contact info
+	phone = models.CharField(max_length=15, null=True, blank=True)
+	qq = models.CharField(max_length=15, null=True, blank=True)
+	wechat = models.CharField(max_length=30, null=True, blank=True)
 
 	def __str__(self):	# __unicode__ on Python 2
 		return 'Profile of user: %s' % self.user.username
@@ -38,3 +43,9 @@ class UserProfile(models.Model):
 User.profile = property(
 	lambda u: UserProfile.objects.get_or_create(user=u)[0]
 )
+
+class UserToUniversity(models.Model):
+	user = models.OneToOneField(User)
+	university = models.OneToOneField(University)
+	start_year = models.IntegerField(default=0)
+	end_year = models.IntegerField(default=0)
